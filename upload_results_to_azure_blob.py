d5f8a15f5b92b0811585f11bdf21ee4f26b7f542
Johan Thoren <johan@thoren.xyz>
2024-01-19 14:03:15 +0100

    Adjust shebang.

--- upload_results_to_azure_blob.py
+++ upload_results_to_azure_blob.py
@@ -1,4 +1,4 @@
-#!/bin/env python3
+#!/usr/bin/env python3
 
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 from azure.core.exceptions import ClientAuthenticationError, ServiceRequestError
